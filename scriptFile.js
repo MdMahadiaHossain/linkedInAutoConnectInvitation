fca06f1c2abd72be45d706072d80fa0fa1919cf7
MdMahadiaHossain <mahadihossainpvt@gmail.com>
2021-04-20 19:50:31 +0600

    Create scriptFile.js

---
+++ scriptFile.js
@@ -0,0 +1,37 @@
+//Got to https://www.linkedin.com/mynetwork/ and run this script to console
+
+
+var buttons = document.getElementsByTagName('button');
+
+var connectButtons = []
+
+for (var i = 0; i < buttons.length; i++) {
+  var text = buttons[i].textContent;
+  if (text.includes('Connect')) {
+    connectButtons.push(buttons[i])
+  }
+
+}
+
+console.log(connectButtons.length)
+
+var total = 0;
+
+setInterval(function() {
+
+  var length = connectButtons.length;
+  if (length != 0 && !document.getElementById('artdeco-modal-outlet')) {
+
+    var connectHead = connectButtons[0];
+    connectButtons.shift();
+    if (total < 90) {
+      connectHead.click();
+      total++
+      console.log("clicked")
+      console.log(total)
+    }
+
+
+  }
+
+}, 3000);
